524a2c79eaba10ff6a1688a0f25756b0f22851aa
Jagadeesh <jagadeesh@Mac.lan>
2026-03-23 18:29:57 +0530

    new files

--- src/com/practise/inteview/programs/java/Anagram.java
+++ src/com/practise/inteview/programs/java/Anagram.java
@@ -7,24 +7,22 @@ import org.testng.annotations.Test;
 public class Anagram {
     
     @Test
-
-    public static char[] ana(String name){
+    public static char[] anagramMethod(String name){
 
         char c[] = name.toCharArray();
-
     Arrays.sort(c);
     return c;
     }
     public static void main(String[] args) {
         
-        char []newone =ana("hello");
-        char []new1=ana("silent");
+        char []newone =anagramMethod("hello");
+        char []new1=anagramMethod("silent");
 
         if(Arrays.equals(newone, new1)){
             System.out.println("anagram");
         }
 else{
     System.out.println("not anagram");
-}    }
-    
-    } 
+}    
+}   
+} 
