5c5a299f7d4d4c17509cc0a677ce28417c5dff58
Jagadeesh <jagadeesh@Jagadeeshs-MacBook-Air.local>
2026-03-28 16:52:42 +0530

    adding new changes

---
+++ src/com/importent/interviewconcepts/java/StringConcp.java
@@ -0,0 +1,21 @@
+package com.importent.interviewconcepts.java;
+
+public class StringConcp {
+
+    public static void main(String[] args) {
+        /*
+
+        String: sequence of characters
+        String buffer: sequence of characters.
+                        mutable and thread-safe
+                        synchronization and multi thread
+        String builder: sequence of characters
+                        is mutable and not thread-safe
+                        and no synchronization
+                        single thread at a time
+                        heavy string manipulation
+                        faster
+         */
+
+    }
+}
