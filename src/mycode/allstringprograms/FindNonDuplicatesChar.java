a04bda49c3bba242005367152b35cab3d2b5198a
jagadeesh <jagadesh017@gmail.com>
2026-04-18 12:05:54 +0530

    new code

--- src/mycode/allstringprograms/FindNonDuplicatesChar.java
+++ src/mycode/allstringprograms/FindNonDuplicatesChar.java
@@ -1,5 +1,20 @@
 package mycode.allstringprograms;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FindNonDuplicatesChar {
 
+    public static void main(String[] args) {
+        String name = "hello";
+        Map<Character, Integer> map = new HashMap<>();
+      for(char c : name.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0)+1);
+          }
+            for(Map.Entry<Character, Integer> entry : map.entrySet()){
+                if(entry.getValue() == 1){
+                    System.out.println(entry.getKey());
+                }
+            }
+        }
 }
